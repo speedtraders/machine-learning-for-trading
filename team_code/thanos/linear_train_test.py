df0a72015cb6bdb0bca3547069a74922bf7dbcf3
ThanAid <thanos.aidinis@gmail.com>
2023-10-26 11:02:48 +0300

    added linear regression approach

--- team_code/thanos/linear_train_test.py
+++ team_code/thanos/linear_train_test.py
@@ -8,27 +8,35 @@ df = pd.read_csv("data\\csvs\\AACG.csv", delimiter=",", index_col=False)
 print(df.head)
 df = df['Open']
 # df.drop(columns=['Ticker'], inplace=True)
+df = df.tail(365)
 
-df_test = df.tail(200)
-df_train = df.drop(df.tail(200).index, inplace=False)
+preds = []
+y = []
+for i in range(333):
 
-y_train = df_train.to_numpy()
-X_train = np.array(range(len(y_train))).reshape(-1, 1)
+    df_test = df.iloc[i+30]
+    df_train = df.iloc[i:i+30]
 
-y_test = df_test.to_numpy().reshape(-1, 1)
-X_test = np.array(range(len(X_train), len(df))).reshape(-1, 1)
+    y_train = df_train.to_numpy()
+    X_train = np.array(range(i, i+30)).reshape(-1, 1)
 
-print(f'Len of train: {len(X_train)} \n len of test: {len(X_test)}')
+    y_test = df_test.reshape(-1, 1)
+    X_test = np.array(i+30).reshape(-1, 1)
 
-reg = LinearRegression()
+    print(f'Len of train: {len(X_train)} \n len of test: {len(X_test)}')
 
-reg.fit(X_train, y_train)
+    reg = LinearRegression()
 
-# prediction
+    reg.fit(X_train, y_train)
+
+    # prediction
+    preds.append(reg.predict(X_test)[0])
+    y.append(y_test[0][0])
+
 df_result = pd.DataFrame()
 # df_result['Days'] = pd.DataFrame(X_test)
-df_result['preds'] = reg.predict(X_test)
-df_result['real'] = pd.DataFrame(y_test)
+df_result['preds'] = preds
+df_result['real'] = y
 
 sns.lineplot(data=df_result)
 plt.show()
